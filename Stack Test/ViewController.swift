f281fccd051e84760e752b077799513e64515a94
Hunter Stanton <hunter_stanton@outlook.com>
2017-02-08 02:07:08 -0500

    Added pushing/popping off the stack

--- Stack Test/ViewController.swift
+++ Stack Test/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var StackTextField: UITextView!
+    
+    // Our stack object that we will push and pop onto
+    var stack: Stack = Stack<Int>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func PrintStackPressed(_ sender: Any)
+    {
+        print(stack)
+    }
 
+    @IBAction func PushPressed(_ sender: Any)
+    {
+        let value: Int = Int(arc4random_uniform(2000))
+        stack.push(value)
+        print("Pushed \(value) onto the stack.")
+    }
+    @IBAction func PopPressed(_ sender: Any)
+    {
+        if(stack.count == 0)
+        {
+            print("There is nothing on the stack.")
+            return
+        }
+        else
+        {
+        let value: Int = stack.pop()!
+        print("Popped \(value) off the stack.")
+        }
+    }
 }
 
